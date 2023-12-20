4a6143b2eb65282c17e5fa13dddc606eb1ee95b5
Ha Nguyen <90432271+nthhaf@users.noreply.github.com>
2023-12-20 22:46:23 +0700

    Update index.jsx

--- src/pages/Library/index.jsx
+++ src/pages/Library/index.jsx
@@ -1,7 +1,7 @@
 const Library = () => {
     return ( 
         <div className="text-7xl h-full justify-center items-center w-full">
-            DM team lol
+            
         </div>
      );
 }
