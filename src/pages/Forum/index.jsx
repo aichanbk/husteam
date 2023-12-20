f546c152ef7d87750e3f3424bc1a2a21a7c7e0fd
Ha Ng <90432271+nthhaf@users.noreply.github.com>
2023-12-20 22:54:34 +0700

    ok

--- src/pages/Forum/index.jsx
+++ src/pages/Forum/index.jsx
@@ -1,7 +1,7 @@
 const Forum = () => {
     return ( 
         <div className="text-7xl h-full justify-center items-center w-full">
-            DM team lol viết lol viết lắm bắt bố m làm
+            Forum
         </div>
      );
 }
